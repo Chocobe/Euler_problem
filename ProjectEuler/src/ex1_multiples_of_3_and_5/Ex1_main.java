71f69f08e6cc5d226abda5c884b6891759dcad01
Chocobe <nice_10bird@naver.com>
2019-04-13 19:39:47 +0900

    <master> : Ex1) multiples of 3 and 5

---
+++ ProjectEuler/src/ex1_multiples_of_3_and_5/Ex1_main.java
@@ -0,0 +1,35 @@
+package ex1_multiples_of_3_and_5;
+
+public class Ex1_main {
+	public static void main(String[] args) {
+		
+		int sum = 0;							// 합계
+		int limit_num = 1000;					// 한계값(미만의 값)
+		int multiple_3 = 3;						// 배수 조건1
+		int multiple_5 = 5;						// 배수 조건2
+		int overlap = multiple_3 * multiple_5;	// 중복값
+		
+		sum += sum_of_multiple_of(multiple_3, limit_num);	// 3의 배수의 총합
+		sum += sum_of_multiple_of(multiple_5, limit_num);	// 5의 배수의 총합		
+		sum -= sum_of_multiple_of(overlap, limit_num);		// 중복값(15) 배제
+		
+		System.out.println("Sum of Multiples of 3 and 5 : " + sum);
+	}
+	
+	/***
+	 * 메서드 : "_val"값의 배수의 총합 (한계값 까지)
+	 * 
+	 * Ex1_main 클래스의 객체를 만들어서 사용하는게 아니기 때문에,
+	 * 정적 메서드로 생성 (static method)
+	 */
+	public static int sum_of_multiple_of(int _val, int _max) {
+		
+		int temp = 0;	// 루프당 해당값 저장
+		
+		for(int i = 0; i * _val < _max; i++) {
+			temp += (i * _val);
+		}
+		
+		return temp;
+	}
+}
---
+++ ProjectEuler/src/ex1_multiples_of_3_and_5/README.md
@@ -0,0 +1,12 @@
+# Multiples of 3 and 5.
+(3과 5의 배수들)
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5,
+we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+(3이나 5의 배수인 10 미만의 자연수를 나열한다면, 3, 5, 6, 9가 된다. 이 배수들의 합은 23이다.)
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+(3이나 5의 배수인 1000 미만의 배수 전체의 합을 구하시오.)
+(추가한 요구사항 : 중복값 배제)
