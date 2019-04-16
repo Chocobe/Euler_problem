edcb71029ead1ad1069f52885536739b78b67431
Chocobe <kyw05171@gmail.com>
2019-04-16 16:04:20 +0900

    <master> : Ex6) sum square difference

---
+++ ProjectEuler/src/ex6_sum_square_difference/Ex6_main.java
@@ -0,0 +1,36 @@
+package ex6_sum_square_difference;
+
+public class Ex6_main {
+	public static void main(String[] args) {
+		
+		int max_num = 100;		// 범위의 최대값
+		
+		int sum = 0;			// 합계
+		int square_of_sum = 0;	// 합의 제곱
+		int sum_of_square = 0;	// 제곱의 합
+		int target_num = 0;		// 대상값
+		
+		int diff_val = 0;		// 차이값(결과값)
+		
+		for(int i = 0; i < max_num; i++) {
+			target_num = max_num - i;
+			
+			sum += target_num;
+			sum_of_square += (int)Math.pow(target_num, 2);
+			
+		}
+		
+		square_of_sum = (int)Math.pow(sum, 2);
+		
+		if(square_of_sum > sum_of_square) {
+			diff_val = square_of_sum - sum_of_square;
+		
+		} else {
+			diff_val = sum_of_square - square_of_sum;
+			
+		}
+		
+		System.out.println("차이값은 : " + diff_val);
+		
+	}
+}
---
+++ ProjectEuler/src/ex6_sum_square_difference/README.md
@@ -0,0 +1,17 @@
+# Sum Square Difference
+# (합 제곱 차이)
+
+The sum of the squares of the first ten natural numbers is 385.
+(첫 10개 자연수의 제곱의 합은 385이다.)
+
+The square of the sum of the first ten natural numbers is 3025
+(첫 10개 자연수의 합의 제곱은 3025 이다.)
+
+Hence the difference between the sum of the squares of the first ten natural numbers and
+the square of the sum is 3025 - 385 = 2640.
+(결과적으로 첫 10개 자연수의 제곱의 합과 합의 제곱 사이의 차이는 2640 = 3025 - 385 이다.
+
+Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+(첫 100개 자연수의 제곱의 합과 합의 제곱 사이의 차를 구해라.)
+
+이번 문제는 쉽다..
