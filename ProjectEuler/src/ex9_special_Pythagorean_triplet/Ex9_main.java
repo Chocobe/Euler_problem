c258fed4d4f6f45452bc5e56ad862b4a16e30a09
Chocobe <kyw05171@gmail.com>
2019-04-17 17:57:22 +0900

    <master> : Ex9) special Pythagorean triplet

---
+++ ProjectEuler/src/ex9_special_Pythagorean_triplet/Ex9_main.java
@@ -0,0 +1,36 @@
+package ex9_special_Pythagorean_triplet;
+
+public class Ex9_main {
+	public static void main(String[] args) {
+		
+		int total_sum = 1000;
+		
+		int val_a = 0;	// 변 a의 값
+		int val_b = 0;	// 변 b의 값
+		int val_c = 0;	// 변 c의 값
+		
+		int limit_a = (total_sum / 3);			// 변 a의 최대값
+		int limit_b = (total_sum - val_a) / 2;	// 변 b의 최대값
+		
+		for(int i = 1; i <= limit_a; i++) {
+			val_a = i;
+			
+			for(int j = (val_a + 1); j <= limit_b; j++) {
+				val_b = j;
+				val_c = total_sum - (val_a + val_b);
+				
+				// a^2 + b^2 = c^2
+				if(Math.pow(val_a, 2) + Math.pow(val_b, 2) == Math.pow(val_c, 2)) {
+					System.out.println("a값 : " + val_a);
+					System.out.println("b값 : " + val_b);
+					System.out.println("c값 : " + val_c);
+					System.out.println("a * b * c 값 : " + val_a * val_b * val_c);
+					
+					return;
+				} 
+			}
+		}
+		
+		System.out.println("없음");		
+	}
+}
---
+++ ProjectEuler/src/ex9_special_Pythagorean_triplet/README.md
@@ -0,0 +1,14 @@
+# Special Pythagorean triplet
+# (특이한 피타고라스 트리플)
+
+A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+(피타고라스 트리플은 "a < b < c" 인 세 자연수의 세트이다.)
+
+a^2 + b^2 = c^2
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+(정확히 "a + b + c = 1000"인 피타고라스 트리플이 있다.) 
+
+Find the product abc.
+("a * b * c"를 찾아라.)
